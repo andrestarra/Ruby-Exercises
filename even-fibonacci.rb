907343ed0e8c7f64eb42a83a2bb9d00afdd4a7c0
AndresFTR <afelipe0113@gmail.com>
2019-01-15 16:03:37 -0500

    Suma de números de fibonacci pares

---
+++ even-fibonacci.rb
@@ -0,0 +1,16 @@
+def suma_fibonacci_pares
+    a = 1
+    b = 1
+    suma_pares = 0
+    while b < 4000000
+        if b % 2 == 0
+            suma_pares += b
+        end
+        c = a + b
+        a = b
+        b = c
+    end
+    puts "La suma de los números pares de la serie de fibonacci hasta 4000000 es: #{suma_pares}"
+end
+
+suma_fibonacci_pares
