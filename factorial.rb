342679bbfd0cdc7353d6f6a01755f5d2d46516ab
AndresFTR <afelipe0113@gmail.com>
2019-01-17 15:30:05 -0500

    Reducción del metodo

--- factorial.rb
+++ factorial.rb
@@ -2,13 +2,15 @@ print "Ingresa un número: "
 num = gets.to_i
 
 def factorial(x)
-    fact = 1
-    x.downto(1) do |i|
-        fact = fact * i
-    end
-    fact
+    x.downto(1).inject {|fact, i| fact * i}
 end
 
 resp = factorial(num)
 
 puts "El factorial de #{num} es #{resp}"
+
+puts factorial(10)==3628800
+puts factorial(30)==265252859812191058636308480000000
+puts factorial(22)==1124000727777607680000
+puts factorial(17)==355687428096000
+puts factorial(9)==362880
