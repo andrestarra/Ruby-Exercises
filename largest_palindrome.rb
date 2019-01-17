427084974bdf9d13a20b7fc901059c4302154353
AndresFTR <afelipe0113@gmail.com>
2019-01-17 16:05:27 -0500

    Retorna resultado en vez de un string

--- largest_palindrome.rb
+++ largest_palindrome.rb
@@ -1,19 +1,17 @@
 def largest_palindrome
     resultado = 0
-    n_uno = 0
-    n_dos = 0
     aux = 0
     999.downto(100) do |i|
         999.downto(100) do |j|
             aux = i * j
             if aux.to_s == aux.to_s.reverse && aux > resultado
                 resultado = aux
-                n_uno = i
-                n_dos = j
             end
         end
     end
-    puts "Mayor número palindromo resultado de un producto de dos números de tres digitos: #{n_uno} * #{n_dos} : #{resultado}"
+    resultado
 end
 
-largest_palindrome
+puts "Mayor número palindromo resultado de un producto de dos números de tres digitos es #{largest_palindrome}"
+
+puts largest_palindrome==906609
