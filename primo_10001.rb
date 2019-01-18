a786981f3dde7218b1e1a0a6b4b0bea5ded22479
AndresFTR <afelipe0113@gmail.com>
2019-01-18 14:20:46 -0500

    Metodos de primos reducidos

--- primo_10001.rb
+++ primo_10001.rb
@@ -1,34 +1,35 @@
 require "prime"
 
-# def es_primo?(x)
-#     div = 0
-#     resp = false
-#     1.upto(x) do |i| 
-#         if x % i == 0 
-#             div += 1
-#         end
-#     end
-#     if div == 2
-#         resp = true
-#     end
-#     resp
-# end
-
-# def primo_10001
-#     cant = 0
-#     primo = 1
-#     while cant < 100001
-#         if es_primo?(primo)
-#             cant += 1
-#         end
-#         primo += 1
-#     end
-#     primo-1
-# end
-
-def primo_10001
-    primos = Prime.first(10001)
-    primos.last
+def es_primo?(x)
+  div = 0
+  (1..x).each do |i| 
+    if x % i == 0 
+      div += 1
+    end
+  end
+  if div == 2 then return true else return false end
 end
 
+def primo_100
+  arr = []
+  primo , i = 1 , 1
+  until arr.length == 100
+    if es_primo?(i)
+      arr << i
+    end
+    i += 1
+  end
+  arr.last
+end
+
+def primo_10001
+    Prime.first(10001).last
+end
+
+puts "Primo #100: #{primo_100}" 
+
 puts "El número primo #10001 es #{primo_10001}"
+
+puts primo_100 == 541
+
+puts primo_10001 == 104743
