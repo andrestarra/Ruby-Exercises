348b0b67ec8780dd73543af30ace7a26f754018c
AndresFTR <afelipe0113@gmail.com>
2019-01-18 15:32:05 -0500

    Pequeños cambios

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -1,22 +1,23 @@
 def largest_prime_factor (x)
-    # aux = x - 1
-    # lpf = 0
-    primos = []
-    aux = x
-    until aux == 1
-        actual = 0
-        i = 2
-        while  actual == 0
-            if aux % i == 0
-                actual = i
-                primos << actual.to_i
-            else
-                i += 1
-            end
-        end
-        aux = aux / actual
+  primos = []
+  aux = x
+  until aux == 1
+    actual , i = 0 , 2
+    while actual == 0
+      if aux % i == 0
+        actual = i
+        primos << actual.to_i
+      else
+        i += 1
+      end
     end
-    puts primos.max
+    aux = aux / actual
+  end
+  primos.max
 end
 
-largest_prime_factor (600851475143)
+puts "El mayor factor primo de 600851475143 es #{largest_prime_factor(600851475143)}"
+
+puts largest_prime_factor(3960) == 11
+puts largest_prime_factor(24) == 3
+puts largest_prime_factor(180) == 5
