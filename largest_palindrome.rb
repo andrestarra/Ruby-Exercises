db2252a339697d2fcf6d74aca97cf588ff16829d
AndresFTR <afelipe0113@gmail.com>
2019-01-18 15:11:42 -0500

    Quitar variable

--- largest_palindrome.rb
+++ largest_palindrome.rb
@@ -1,15 +1,14 @@
 def largest_palindrome
-    resultado = 0
-    aux = 0
-    999.downto(100) do |i|
-        999.downto(100) do |j|
-            aux = i * j
-            if aux.to_s == aux.to_s.reverse && aux > resultado
-                resultado = aux
-            end
-        end
+  resultado = 0
+  999.downto(100) do |i|
+    999.downto(100) do |j|
+    aux = i * j
+      if aux.to_s == aux.to_s.reverse && aux > resultado
+        resultado = aux.to_i
+      end
     end
-    resultado
+  end
+  resultado
 end
 
 puts "Mayor número palindromo resultado de un producto de dos números de tres digitos es #{largest_palindrome}"
