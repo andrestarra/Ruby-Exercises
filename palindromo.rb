d258244fd4c3dabd8e417f90a12951842973cb21
AndresFTR <afelipe0113@gmail.com>
2019-01-18 11:34:20 -0500

    Cambios en lo que retorna el metodo

--- palindromo.rb
+++ palindromo.rb
@@ -2,11 +2,15 @@ print "Ingresa una palabra: "
 palabra = gets.chomp
 
 def es_palindromo?(palabra)
-    if palabra == palabra.reverse
-        puts "La palabra #{palabra} es palíndromo."
-    else
-        puts "La palabra #{palabra} NO es palíndromo"
-    end
+	if palabra == palabra.reverse then return true else return false end
 end
 
-es_palindromo?(palabra)
+str = "La palabra #{palabra}"
+if es_palindromo?(palabra) then puts "#{str} es palindromo" else puts "#{str} NO es palindromo" end
+
+puts es_palindromo?("abalaba")==true
+puts es_palindromo?("ana")==true
+puts es_palindromo?("salas")==true
+puts es_palindromo?("andres")==false
+puts es_palindromo?("somos")==true
+
