cc771520a7d62e3b46cb179d776d185b85815110
AndresFTR <afelipe0113@gmail.com>
2019-01-18 16:40:47 -0500

    Utilizar clase Prime.prime? de ruby en el metodo hecho y arreglar if then

--- primo_10001.rb
+++ primo_10001.rb
@@ -1,20 +1,10 @@
 require "prime"
 
-def es_primo?(x)
-  div = 0
-  (1..x).each do |i| 
-    if x % i == 0 
-      div += 1
-    end
-  end
-  if div == 2 then return true else return false end
-end
-
-def primo_100
+def primo_2
   arr = []
   primo , i = 1 , 1
-  until arr.length == 100
-    if es_primo?(i)
+  until arr.length == 10001
+    if Prime.prime?(i)
       arr << i
     end
     i += 1
@@ -26,10 +16,10 @@ def primo_10001
     Prime.first(10001).last
 end
 
-puts "Primo #100: #{primo_100}" 
+puts "Primo #10001: #{primo_2}" 
 
 puts "El número primo #10001 es #{primo_10001}"
 
-puts primo_100 == 541
+puts primo_2 == 104743
 
 puts primo_10001 == 104743
