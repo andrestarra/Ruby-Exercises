326eddfe656ce4cadd69edc170aa26b053443230
AndresFTR <afelipe0113@gmail.com>
2019-01-21 16:58:43 -0500

    Test de la diferencia de las sumas de cuadrados

--- dif_suma_cuadrados.rb
+++ dif_suma_cuadrados.rb
@@ -1,14 +1,30 @@
+require "minitest/autorun"
+
 def suma_cuadrados
-    (1..100).inject {|sum, n| sum + (n ** 2) }
+  (1..100).inject { |sum, n| sum + (n ** 2) }
 end
 
 def cuadrado_de_suma
-    suma = (1..100).reduce(:+)
-    suma ** 2
+  suma = (1..100).reduce(:+)
+  suma ** 2
 end
 
 def dif_sumas
-    cuadrado_de_suma - suma_cuadrados
+  cuadrado_de_suma - suma_cuadrados
 end
 
 puts "#{suma_cuadrados} - #{cuadrado_de_suma} = #{dif_sumas}"
+
+class TestDifSumaCuadrados < Minitest::Test
+  def test_suma_cuadrados
+    assert_equal 338350, suma_cuadrados
+  end
+
+  def test_cuadrado_suma
+    assert_equal 25502500, cuadrado_de_suma
+  end
+
+  def test_dif_sumas
+    assert_equal 25164150, dif_sumas
+  end
+end
--- multiples3-5.rb
+++ multiples3-5.rb
@@ -30,15 +30,19 @@ class TestUno < Minitest::Test
   def test_multiplo_3
     assert_equal true, es_multiplo_3?(6)
   end
+
   def test_multiplo_3_2
     assert_equal true, es_multiplo_3?(12)
   end
+
   def test_multiplo_5
     assert_equal true, es_multiplo_5?(10)
   end
+
   def test_multiplo_5_2
     assert_equal true, es_multiplo_5?(20)
   end
+  
   def test_multiplo_3_5
     assert_equal 234168, multiplos_3_5
   end
